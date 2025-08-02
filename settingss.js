5cb246fdbb1dbc833834ae75f3e8116d2319f8d8
YUUICHI6 <151581959+YUUICHI6@users.noreply.github.com>
2025-08-02 19:15:38 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: " malvin~lzMk0ATY#F191_K0D-n6Wmj8ZXhzbzJhjypopQnCZdy4wKPa93Rg",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "2250171833953", // PUT OWNER NUMBER HERE
+  PREFIX: "¡" // prefix (e.g., ., /, !, *)
 };
